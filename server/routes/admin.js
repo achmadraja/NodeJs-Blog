c6d22bcbfa75ee745b7bac93748c1f90754e4e71
achmadraja <achmadrajaqz@gmail.com>
2025-05-03 13:40:07 +0700

    fix: update admin loogin

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -5,84 +5,119 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-
 const adminLayout = '../views/layouts/admin';
+const jwtSecret = process.env.JWT_SECRET;
+
+
 /**
- * GET/
+ * 
+ * Check Login
+*/
+const authMiddleware = (req, res, next ) => {
+  const token = req.cookies.token;
+
+  if(!token) {
+    return res.status(401).json( { message: 'Unauthorized'} );
+  }
+
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    req.userId = decoded.userId;
+    next();
+  } catch(error) {
+    res.status(401).json( { message: 'Unauthorized'} );
+  }
+}
+
+
+/**
+ * GET /
  * Admin - Login Page
- */
+*/
 router.get('/admin', async (req, res) => {
-    try {
-        const locals = {
-            title: "NodeJs Blog",
-            description: "Simple Blog created with NodeJs, Express & MongoDB."
-        }
-
-        res.render('admin/index', { locals, layout: adminLayout });
-    }
-    catch (error) {
-        console.log(error);
+  try {
+    const locals = {
+      title: "Admin",
+      description: "Simple Blog created with NodeJs, Express & MongoDb."
     }
 
+    res.render('admin/index', { locals, layout: adminLayout });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 
 /**
- * POST/
+ * POST /
  * Admin - Check Login
- */
+*/
 router.post('/admin', async (req, res) => {
-    try {
-        const { username, password } = req.body;
+  try {
+    const { username, password } = req.body;
+    
+    const user = await User.findOne( { username } );
 
-        // Find the user in the database
-        const user = await User.findOne({ username });
-        if (!user) {
-            return res.status(401).send('Wrong username or password.');
-        }
-
-        // Compare the provided password with the hashed password in the database
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if (!isPasswordValid) {
-            return res.status(401).send('Wrong username or password.');
-        }
-
-        // Generate a JWT token (optional, for session management)
-        const token = jwt.sign({ id: user._id, username: user.username }, 'your_secret_key', { expiresIn: '1h' });
-
-        res.status(200).json({ message: 'You are logged in.', token });
-    } catch (error) {
-        console.log(error);
-        res.status(500).send('Internal Server Error');
+    if(!user) {
+      return res.status(401).json( { message: 'Invalid credentials' } );
     }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if(!isPasswordValid) {
+      return res.status(401).json( { message: 'Invalid credentials' } );
+    }
+
+    const token = jwt.sign({ userId: user._id}, jwtSecret );
+    res.cookie('token', token, { httpOnly: true });
+    res.redirect('/dashboard');
+
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 
+// router.post('/admin', async (req, res) => {
+//   try {
+//     const { username, password } = req.body;
+    
+//     if(req.body.username === 'admin' && req.body.password === 'password') {
+//       res.send('You are logged in.')
+//     } else {
+//       res.send('Wrong username or password');
+//     }
+
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
+
+
 /**
- * POST/
+ * POST /
  * Admin - Register
- */
+*/
 router.post('/register', async (req, res) => {
     try {
-       const { username, password } = req.body;
-       const hashedPassword = await bcrypt.hash(password, 10);
-
-       try {
-           const user = await User.create({ username, password: hashedPassword });
-           res.status(201).json({message: 'User Created', user});
-       } catch (error) {
-           if(error.code === 11000) {
-               res.status(409).json({ message: 'Username already exists' });
-           }
-           res.status(500).json({ message: 'Internal Server Error' });
-       }
-         
-
+      const { username, password } = req.body;
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+      try {
+        const user = await User.create({ username, password:hashedPassword });
+        res.status(201).json({ message: 'User Created', user });
+      } catch (error) {
+        if(error.code === 11000) {
+          res.status(409).json({ message: 'User already in use'});
+        }
+        res.status(500).json({ message: 'Internal server error'})
+      }
+  
+    } catch (error) {
+      console.log(error);
     }
-    catch (error) {
-        console.log(error);
-    }
-});
+  });
+  
 
 
 module.exports = router;
